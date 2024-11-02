d92f406c2994c560819afcdcabad4dec858fbf51
Luis Fernando <guffenix@gmail.com>
2024-11-02 14:41:03 -0500

    refactor: enable usefull info

--- src/app/components/header/header.component.tsx
+++ src/app/components/header/header.component.tsx
@@ -1,9 +1,11 @@
 'use client'
 
-import { Search, Menu, X } from 'lucide-react'
+import { Search, Menu, X, Copy } from 'lucide-react'
 import Link from 'next/link'
 import Image from 'next/image'
 import { useState } from 'react'
+import { ADDRESS_OWNER, truncateAddress } from '~/lib/utils'
+import { Button } from '~/components/ui/button'
 
 export function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
@@ -47,23 +49,16 @@ export function Header() {
             {isMenuOpen ? <X size={24} /> : <Menu size={24} />}
           </button>
           <div className="hidden lg:flex items-center space-x-4">
-            {/* <div className="relative">
-              <input
-              className="w-64 bg-[#0a192f] border-[#f26419] text-white placeholder-gray-500 p-2 outline outline-[#f26419]"
-              placeholder="Search"
-              />
-              <Search
-              className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500"
-              size={18}
-              />
-              </div> */}
             <div className="flex items-center space-x-2">
               <img src="/img/star.png" className="w-10 h-9" alt="star" />
-              <span className="font-semibold">10000</span>
+              <code className="relative rounded px-[0.3rem] py-[0.2rem] font-mono text-sm bg-secondary/10 hover:bg-secondary/20">
+                {truncateAddress(ADDRESS_OWNER)}
+              </code>
+              <Button variant="ghost" size="icon" className="h-8 w-8">
+                <Copy className="h-4 w-4" />
+                <span className="sr-only">Copy address</span>
+              </Button>
             </div>
-            {/* <button className="bg-[#f26419] hover:bg-[#f26419]/90 text-white px-4 py-2">
-              Connect Wallet
-            </button> */}
           </div>
         </div>
       </div>
@@ -110,7 +105,13 @@ export function Header() {
         <div className="flex items-center justify-between">
           <div className="flex items-center space-x-2">
             <img src="/img/star.png" className="w-10 h-9" alt="star" />
-            <span className="font-semibold">10000</span>
+            <code className="relative rounded px-[0.3rem] py-[0.2rem] font-mono text-sm bg-secondary/10 hover:bg-secondary/20">
+              {truncateAddress(ADDRESS_OWNER)}
+            </code>
+            <Button variant="ghost" size="icon" className="h-8 w-8">
+              <Copy className="h-4 w-4" />
+              <span className="sr-only">Copy address</span>
+            </Button>
           </div>
           <button className="bg-[#f26419] hover:bg-[#f26419]/90 text-white px-4 py-2">
             Connect Wallet
