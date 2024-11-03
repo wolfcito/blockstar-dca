29858fa83f81784d9d242effbc90566137c32022
Luis Fernando Ushiña <guffenix@gmail.com>
2024-11-03 16:47:01 -0500

    chore: Update learn page

--- src/app/learn/page.tsx
+++ src/app/learn/page.tsx
@@ -1,7 +1,10 @@
+import { Loading } from '~/components/loading'
+
 export default function LearnPage() {
   return (
     <div className="max-w-2xl mx-auto text-white p-4">
-      <h1 className="text-4xl font-bold text-center mb-8">Learn</h1>
+      <h1 className="text-4xl font-bold text-center mb-8">Begin to learn how to invest wisely</h1>
+        <Loading />
     </div>
   )
 }
