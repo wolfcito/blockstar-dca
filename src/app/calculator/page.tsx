6b04635b0c3ac7c19b02b407c239748465bbcecc
Luis Fernando Ushiña <guffenix@gmail.com>
2024-11-03 16:48:17 -0500

    chore: Update calculator

--- src/app/calculator/page.tsx
+++ src/app/calculator/page.tsx
@@ -1,7 +1,10 @@
+import { Loading } from '~/components/loading'
+
 export default function CalculatorPage() {
   return (
     <div className="max-w-2xl mx-auto text-white p-4">
       <h1 className="text-4xl font-bold text-center mb-8">Calculator</h1>
+      <Loading />
     </div>
   )
 }
